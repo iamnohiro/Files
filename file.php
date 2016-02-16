d133afefaeb7af5dcd334359a4e1559af877edb0
Alen <iamnohiro@gmail.com>
2016-02-16 11:34:37 +0800

    File Added

---
+++ file.php
@@ -0,0 +1,116 @@
+<div class="properties-content">
+					<article id="property-7818" class="post-7818 noo_property type-noo_property status-publish has-post-thumbnail hentry property_category-single-family-home property_location-coral-gables property_status-sales has-featured">
+			    <div class="property-featured">
+				<a class="content-thumb" href="http://www.saggiorealty.com/properties/630-leucadendra-dr/">
+						<img width="370" height="300" src="http://www.saggiorealty.com/wp-content/uploads/2016/01/0-full-2-370x300.jpg" class="attachment-property-thumb size-property-thumb wp-post-image" alt="0-full" />					</a>
+															<span class="property-category"><a href="http://www.saggiorealty.com/listings/single-family-home/" rel="tag">Single Family Home</a></span>
+			    </div>
+				<div class="property-wrap">
+					<h2 class="property-title">
+						<a href="http://www.saggiorealty.com/properties/630-leucadendra-dr/" title="Permanent link to: &quot;630 LEUCADENDRA DR&quot;">630 LEUCADENDRA DR</a>
+											</h2>
+					<div class="property-excerpt">
+											</div>
+					<div class="property-summary">
+						<div class="property-detail">
+<div class="size"><span>12098 m</span></div>
+<div class="bathrooms"><span>11</span></div>
+<div class="bedrooms"><span>7</span></div>
+</div>						<div class="property-info">
+							<div class="property-price">
+								<span>18,900,000 </span>
+							</div>
+							<div class="property-action">
+								<a href="http://www.saggiorealty.com/properties/630-leucadendra-dr/">More Details</a>
+							</div>
+						</div>
+						<div class="property-info property-fullwidth-info">
+							<div class="property-price">
+								<span>18,900,000 </span>
+							</div>
+							<div class="size"><span>12098 m</span></div>
+<div class="bathrooms"><span>11</span></div>
+<div class="bedrooms"><span>7</span></div>						</div>
+					</div>
+				</div>
+				<div class="property-action property-fullwidth-action">
+					<a href="http://www.saggiorealty.com/properties/630-leucadendra-dr/">More Details</a>
+				</div>
+			</article> <!-- /#post- -->
+				<article id="property-7815" class="post-7815 noo_property type-noo_property status-publish has-post-thumbnail hentry property_category-single-family-home property_location-coral-gables property_status-sales has-featured">
+			    <div class="property-featured">
+			    				        <a class="content-thumb" href="http://www.saggiorealty.com/properties/181-leucadendra-dr/">
+						<img width="370" height="300" src="http://www.saggiorealty.com/wp-content/uploads/2016/01/2-370x300.jpg" class="attachment-property-thumb size-property-thumb wp-post-image" alt="2" />					</a>
+															<span class="property-category"><a href="http://www.saggiorealty.com/listings/single-family-home/" rel="tag">Single Family Home</a></span>
+			    </div>
+				<div class="property-wrap">
+					<h2 class="property-title">
+						<a href="http://www.saggiorealty.com/properties/181-leucadendra-dr/" title="Permanent link to: &quot;181 LEUCADENDRA DR&quot;">181 LEUCADENDRA DR</a>
+											</h2>
+					<div class="property-excerpt">
+											</div>
+					<div class="property-summary">
+						<div class="property-detail">
+<div class="size"><span>14946 m</span></div>
+<div class="bathrooms"><span>8</span></div>
+<div class="bedrooms"><span>7</span></div>
+</div>						<div class="property-info">
+							<div class="property-price">
+								<span>19,900,000 </span>
+							</div>
+							<div class="property-action">
+								<a href="http://www.saggiorealty.com/properties/181-leucadendra-dr/">More Details</a>
+							</div>
+						</div>
+						<div class="property-info property-fullwidth-info">
+							<div class="property-price">
+								<span>19,900,000 </span>
+							</div>
+							<div class="size"><span>14946 m</span></div>
+<div class="bathrooms"><span>8</span></div>
+<div class="bedrooms"><span>7</span></div>						</div>
+					</div>
+				</div>
+				<div class="property-action property-fullwidth-action">
+					<a href="http://www.saggiorealty.com/properties/181-leucadendra-dr/">More Details</a>
+				</div>
+			</article> <!-- /#post- -->
+				<article id="property-7813" class="post-7813 noo_property type-noo_property status-publish has-post-thumbnail hentry property_category-single-family-home property_location-coral-gables property_status-sales has-featured">
+			    <div class="property-featured">
+			    				        <a class="content-thumb" href="http://www.saggiorealty.com/properties/110-arvida-pw/">
+						<img width="370" height="300" src="http://www.saggiorealty.com/wp-content/uploads/2016/01/0-370x300.jpg" class="attachment-property-thumb size-property-thumb wp-post-image" alt="0" />					</a>
+															<span class="property-category"><a href="http://www.saggiorealty.com/listings/single-family-home/" rel="tag">Single Family Home</a></span>
+			    </div>
+				<div class="property-wrap">
+					<h2 class="property-title">
+						<a href="http://www.saggiorealty.com/properties/110-arvida-pw/" title="Permanent link to: &quot;110 ARVIDA PW&quot;">110 ARVIDA PW</a>
+											</h2>
+					<div class="property-excerpt">
+											</div>
+					<div class="property-summary">
+						<div class="property-detail">
+<div class="size"><span>12756 m</span></div>
+<div class="bathrooms"><span>10</span></div>
+<div class="bedrooms"><span>6</span></div>
+</div>						<div class="property-info">
+							<div class="property-price">
+								<span>35,000,000 </span>
+							</div>
+							<div class="property-action">
+								<a href="http://www.saggiorealty.com/properties/110-arvida-pw/">More Details</a>
+							</div>
+						</div>
+						<div class="property-info property-fullwidth-info">
+							<div class="property-price">
+								<span>35,000,000 </span>
+							</div>
+							<div class="size"><span>12756 m</span></div>
+<div class="bathrooms"><span>10</span></div>
+<div class="bedrooms"><span>6</span></div>						</div>
+					</div>
+				</div>
+				<div class="property-action property-fullwidth-action">
+					<a href="http://www.saggiorealty.com/properties/110-arvida-pw/">More Details</a>
+				</div>
+			</article> <!-- /#post- -->
+				</div>
